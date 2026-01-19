e0992a30f553d118250ea1319283eca7342f9cc6
ohirume <0uhirume@gmail.com>
2026-01-19 23:32:41 +0800

    feat: add player constants

---
+++ src/shared/constants/player.ts
@@ -0,0 +1,16 @@
+export const DEFAULT_HEALTH = 100;
+export const DEFAULT_MAX_HEALTH = 100;
+
+export const DEFAULT_STRENGTH = 0;
+export const DEFAULT_STRENGTH_MULTIPLIER = 1;
+export const DEFAULT_ENDURANCE = 0;
+export const DEFAULT_ENDURANCE_MULTIPLIER = 1;
+export const DEFAULT_SPEED = 0;
+export const DEFAULT_SPEED_MULTIPLIER = 1;
+export const DEFAULT_JUMP_FORCE = 0;
+export const DEFAULT_JUMP_FORCE_MULTIPLIER = 1;
+export const DEFAULT_POWER = 0;
+export const DEFAULT_POWER_MULTIPLIER = 1;
+
+export const TRAINING_COOLDOWN = 1;
+export const COOLDOWN_ZERO = 0;
