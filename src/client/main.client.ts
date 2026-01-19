190125cbbd052edef57befa203ae5432e712732c
ohirume <0uhirume@gmail.com>
2026-01-19 23:34:12 +0800

    feat: add client entry point
    - setup scheduler
    - setup jabby applet bind

---
+++ src/client/main.client.ts
@@ -0,0 +1,24 @@
+import { ContextActionService } from "@rbxts/services";
+import jabby from "@rbxts/jabby";
+import Net from "@rbxts/yetanothernet";
+import { routes } from "../shared/routes";
+import { scheduler } from "../shared/scheduler";
+import { Plugin as RunServicePlugin } from "@rbxts/planck-runservice";
+import { processInputsSystem } from "./systems/process-inputs";
+
+const [beginFrame, endFrame] = Net.createHook(routes);
+
+scheduler.addPlugin(new RunServicePlugin()).addSystem(beginFrame).addSystem(processInputsSystem).addSystem(endFrame);
+
+const client = jabby.obtain_client();
+
+ContextActionService.BindAction(
+    "Open Jabby Home",
+    (_, state) => {
+        if (state === Enum.UserInputState.Begin) {
+            client.spawn_app(client.apps.home);
+        }
+    },
+    false,
+    Enum.KeyCode.F4,
+);
