960f4690759f7b7cab73859aafc350f29ce1f2ed
ohirume <0uhirume@gmail.com>
2026-01-20 23:13:14 +0800

    feat: wire up client replication

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -3,18 +3,36 @@ import { Plugin as RunServicePlugin } from "@rbxts/planck-runservice";
 import { ContextActionService } from "@rbxts/services";
 import Net from "@rbxts/yetanothernet";
 
+import { replicator } from "../shared/replicator/client";
 import { routes } from "../shared/routes";
 import { scheduler } from "../shared/scheduler";
+import { world } from "../shared/world";
 
 import { processInputsSystem } from "./systems/process-inputs";
+import {
+    replecsReceiveFullSystem,
+    replecsStartReplicationSystem,
+    replecsReceiveUpdateSystem,
+} from "./systems/replecs";
+
+replicator.init(world);
 
 const [beginFrame, endFrame] = Net.createHook(routes);
 
 scheduler
     .addPlugin(new RunServicePlugin())
     .addSystem(beginFrame)
+    .addSystem(replecsReceiveFullSystem)
+    .addSystem(replecsReceiveUpdateSystem)
+    .addSystem(replecsStartReplicationSystem)
     .addSystem(processInputsSystem)
     .addSystem(endFrame);
+
+jabby.register({
+    applet: jabby.applets.world,
+    configuration: { world: world },
+    name: "Client World",
+});
 
 const client = jabby.obtain_client();
 
