1c3c2424a7ffd0e17170576ac41526616599d294
ohirume <0uhirume@gmail.com>
2026-01-20 23:13:28 +0800

    feat: wire up server replication

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -2,20 +2,26 @@ import jabby from "@rbxts/jabby";
 import { Plugin as RunServicePlugin } from "@rbxts/planck-runservice";
 import Net from "@rbxts/yetanothernet";
 
+import { replicator } from "../shared/replicator/server";
 import { routes } from "../shared/routes";
 import { scheduler } from "../shared/scheduler";
-import { sharedWorld } from "../shared/world";
+import { world } from "../shared/world";
 
 import { decreaseCoolDownSystem } from "./systems/decrease-cooldown";
 import { playerJoinedSystem } from "./systems/player-join";
 import { playerLeftSystem } from "./systems/player-left";
+import { replecsHydrateSystem, replecsStartSystem } from "./systems/replecs";
 import { trainSystem } from "./systems/train";
+
+replicator.init(world);
 
 const [beginFrame, endFrame] = Net.createHook(routes);
 
 scheduler
     .addPlugin(new RunServicePlugin())
     .addSystem(beginFrame)
+    .addSystem(replecsStartSystem)
+    .addSystem(replecsHydrateSystem)
     .addSystem(playerJoinedSystem)
     .addSystem(playerLeftSystem)
     .addSystem(trainSystem)
@@ -24,6 +30,6 @@ scheduler
 
 jabby.register({
     applet: jabby.applets.world,
-    configuration: { world: sharedWorld },
-    name: "Jecs World",
+    configuration: { world: world },
+    name: "Server World",
 });
