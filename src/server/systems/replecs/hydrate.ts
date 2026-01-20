83922901a75d0519eaf7828c8d23b1a2d0767492
ohirume <0uhirume@gmail.com>
2026-01-20 23:12:50 +0800

    feat: add server side replecs systems

---
+++ src/server/systems/replecs/hydrate.ts
@@ -0,0 +1,15 @@
+import type { World } from "@rbxts/jecs";
+import type { SystemTable } from "@rbxts/planck";
+
+import { replicator } from "../../../shared/replicator/server";
+import { routes } from "../../../shared/routes";
+
+function system(): void {
+    for (const [player, buf, variants] of replicator.collect_updates()) {
+        routes.receiveUpdate.send(buf, variants).to(player);
+    }
+}
+
+export const replecsHydrateSystem: SystemTable<[World]> = {
+    system,
+};
---
+++ src/server/systems/replecs/start.ts
@@ -0,0 +1,20 @@
+import type { World } from "@rbxts/jecs";
+import type { SystemTable } from "@rbxts/planck";
+
+import { replicator } from "../../../shared/replicator/server";
+import { routes } from "../../../shared/routes";
+
+function system(): void {
+    for (const [_, player] of routes.startReplication.query()) {
+        if (!typeIs(player, "Instance") || !player.IsA("Player")) {
+            continue;
+        }
+
+        const [buf, variants] = replicator.get_full(player);
+        routes.receiveFull.send(buf, variants).to(player);
+    }
+}
+
+export const replecsStartSystem: SystemTable<[World]> = {
+    system,
+};
