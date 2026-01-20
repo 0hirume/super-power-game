880eadce98fe2e112df6b039c9705fc857a1e8cb
ohirume <0uhirume@gmail.com>
2026-01-20 23:11:22 +0800

    feat: use component factory

--- src/shared/components.ts
+++ src/shared/components.ts
@@ -1,35 +1,20 @@
-import { Name } from "@rbxts/jecs";
+import { makeComponent } from "./utilities/component";
+import { world } from "./world";
 
-import { sharedWorld } from "./world";
+export const PlayerInstance = makeComponent<Player>(world, "PlayerInstance", true);
 
-export const PlayerInstance = sharedWorld.component<Player>();
-sharedWorld.set(PlayerInstance, Name, "PlayerInstance");
+export const Health = makeComponent<number>(world, "Health", true);
+export const MaxHealth = makeComponent<number>(world, "MaxHealth", true);
 
-export const Health = sharedWorld.component<number>();
-sharedWorld.set(Health, Name, "Health");
-export const MaxHealth = sharedWorld.component<number>();
-sharedWorld.set(MaxHealth, Name, "MaxHealth");
+export const Strength = makeComponent<number>(world, "Strength", true);
+export const StrengthMultiplier = makeComponent<number>(world, "StrengthMultiplier", true);
+export const Endurance = makeComponent<number>(world, "Endurance", true);
+export const EnduranceMultiplier = makeComponent<number>(world, "EnduranceMultiplier", true);
+export const Speed = makeComponent<number>(world, "Speed", true);
+export const SpeedMultiplier = makeComponent<number>(world, "SpeedMultiplier", true);
+export const JumpForce = makeComponent<number>(world, "JumpForce", true);
+export const JumpForceMultiplier = makeComponent<number>(world, "JumpForceMultiplier", true);
+export const Power = makeComponent<number>(world, "Power", true);
+export const PowerMultiplier = makeComponent<number>(world, "PowerMultiplier", true);
 
-export const Strength = sharedWorld.component<number>();
-sharedWorld.set(Strength, Name, "Strength");
-export const StrengthMultiplier = sharedWorld.component<number>();
-sharedWorld.set(StrengthMultiplier, Name, "StrengthMultiplier");
-export const Endurance = sharedWorld.component<number>();
-sharedWorld.set(Endurance, Name, "Endurance");
-export const EnduranceMultiplier = sharedWorld.component<number>();
-sharedWorld.set(EnduranceMultiplier, Name, "EnduranceMultiplier");
-export const Speed = sharedWorld.component<number>();
-sharedWorld.set(Speed, Name, "Speed");
-export const SpeedMultiplier = sharedWorld.component<number>();
-sharedWorld.set(SpeedMultiplier, Name, "SpeedMultiplier");
-export const JumpForce = sharedWorld.component<number>();
-sharedWorld.set(JumpForce, Name, "JumpForce");
-export const JumpForceMultiplier = sharedWorld.component<number>();
-sharedWorld.set(JumpForceMultiplier, Name, "JumpForceMultiplier");
-export const Power = sharedWorld.component<number>();
-sharedWorld.set(Power, Name, "Power");
-export const PowerMultiplier = sharedWorld.component<number>();
-sharedWorld.set(PowerMultiplier, Name, "PowerMultiplier");
-
-export const CoolDown = sharedWorld.component<number>();
-sharedWorld.set(CoolDown, Name, "CoolDown");
+export const CoolDown = makeComponent<number>(world, "CoolDown", true);
