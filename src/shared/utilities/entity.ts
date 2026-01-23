35cddebeb6505ba3797b23c2ad0e8a96cd9caabd
ohirume <0uhirume@gmail.com>
2026-01-23 11:36:02 +0800

    feat: add component / pair / tag helper for replication

--- src/shared/utilities/entity.ts
+++ src/shared/utilities/entity.ts
@@ -1,4 +1,6 @@
+import type { InferComponent, Pair } from "@rbxts/jecs";
 import type { Entity, World } from "@rbxts/jecs";
+import jecs from "@rbxts/jecs";
 import replecs from "@rbxts/replecs";
 
 export function makeEntity(world: World, isNetworked = false): Entity {
@@ -9,4 +11,55 @@ export function makeEntity(world: World, isNetworked = false): Entity {
     }
 
     return entity;
+}
+
+export function addTag(world: World, entity: Entity, tag: Entity, isReplicated = false): void {
+    world.add(entity, tag);
+
+    if (isReplicated) {
+        world.set(entity, jecs.pair(replecs.reliable, tag), undefined);
+    }
+}
+
+export function setComponent<T>(
+    world: World,
+    entity: Entity,
+    component: Entity<T>,
+    value: InferComponent<Entity<T>>,
+    isReplicated = false,
+): void {
+    world.set(entity, component, value);
+
+    if (isReplicated) {
+        world.set(entity, jecs.pair(replecs.reliable, component), undefined);
+    }
+}
+
+export function addPair(
+    world: World,
+    entity: Entity,
+    relation: Entity,
+    target: Entity,
+    isReplicated = false,
+): void {
+    world.add(entity, jecs.pair(relation, target));
+
+    if (isReplicated) {
+        world.set(entity, jecs.pair(replecs.pair, relation), undefined);
+    }
+}
+
+export function setPairValue<R, T>(
+    world: World,
+    entity: Entity,
+    relation: Entity<R>,
+    target: Entity<T>,
+    value: InferComponent<Pair<R, T>>,
+    isReplicated = false,
+): void {
+    world.set(entity, jecs.pair(relation, target), value);
+
+    if (isReplicated) {
+        world.set(entity, jecs.pair(replecs.pair, relation), undefined);
+    }
 }
