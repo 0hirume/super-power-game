42748cb49b994b0e455b87c5630c9b97ca28b34d
ohirume <0uhirume@gmail.com>
2026-01-25 17:31:00 +0800

    chore: sort routes and fix generic type

--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,13 +1,14 @@
 import { Route } from "@rbxts/yetanothernet";
 
 export const routes = {
+    humanoidJumped: new Route<[]>(),
+
     receiveFull: new Route<[buffer, unknown[][]]>(),
     receiveUpdate: new Route<[buffer, unknown[][]]>(),
-    startReplication: new Route(),
 
-    trainEndurance: new Route(),
-    trainPower: new Route(),
-    trainStrength: new Route(),
+    startReplication: new Route<[]>(),
 
-    humanoidJumped: new Route(),
+    trainEndurance: new Route<[]>(),
+    trainPower: new Route<[]>(),
+    trainStrength: new Route<[]>(),
 };
