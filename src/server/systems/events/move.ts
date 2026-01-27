4d3ece0e813d9eb7ad5997887b7adee816de0f90
ohirume <0uhirume@gmail.com>
2026-01-27 17:01:30 +0800

    feat: use humanoid instance components in place of manual indexing

--- src/server/systems/events/move.ts
+++ src/server/systems/events/move.ts
@@ -1,7 +1,7 @@
 import type { World } from "@rbxts/jecs";
 import { timePassed, type SystemTable } from "@rbxts/planck";
 
-import { PlayerInstance, Speed } from "../../../shared/components";
+import { HumanoidInstance, Speed } from "../../../shared/components";
 import { TrainEvent } from "../../../shared/tags";
 import { addPair } from "../../../shared/utilities/ecs";
 
@@ -9,14 +9,8 @@ const THRESHOLD = 0.5;
 const INTERVAL = 1;
 
 function system(world: World): void {
-    for (const [entity, instance] of world.query(PlayerInstance)) {
-        const humanoid = instance.Character?.FindFirstChildWhichIsA("Humanoid");
-
-        if (humanoid === undefined) {
-            continue;
-        }
-
-        if (humanoid.MoveDirection.Magnitude <= THRESHOLD) {
+    for (const [entity, instance] of world.query(HumanoidInstance)) {
+        if (instance.MoveDirection.Magnitude <= THRESHOLD) {
             continue;
         }
 
--- src/server/systems/humanoid/sync.ts
+++ src/server/systems/humanoid/sync.ts
@@ -1,7 +1,7 @@
 import type { World } from "@rbxts/jecs";
 import type { SystemTable } from "@rbxts/planck";
 
-import { Endurance, Health, JumpForce, PlayerInstance, Speed } from "../../../shared/components";
+import { Endurance, Health, HumanoidInstance, JumpForce, Speed } from "../../../shared/components";
 import { BASE_JUMPHEIGHT, BASE_MAX_HEALTH, BASE_WALKSPEED } from "../../../shared/constants/player";
 
 function system(world: World): void {
@@ -12,20 +12,14 @@ function system(world: World): void {
         enduranceValue,
         speedValue,
         jumpForceValue,
-    ] of world.query(PlayerInstance, Health, Endurance, Speed, JumpForce)) {
-        const humanoid = instance.Character?.FindFirstChildWhichIsA("Humanoid");
+    ] of world.query(HumanoidInstance, Health, Endurance, Speed, JumpForce)) {
+        instance.UseJumpPower = false;
 
-        if (humanoid === undefined) {
-            continue;
-        }
+        instance.Health = healthValue;
+        instance.MaxHealth = BASE_MAX_HEALTH + enduranceValue;
 
-        humanoid.UseJumpPower = false;
-
-        humanoid.Health = healthValue;
-        humanoid.MaxHealth = BASE_MAX_HEALTH + enduranceValue;
-
-        humanoid.WalkSpeed = BASE_WALKSPEED + speedValue;
-        humanoid.JumpHeight = BASE_JUMPHEIGHT + jumpForceValue;
+        instance.WalkSpeed = BASE_WALKSPEED + speedValue;
+        instance.JumpHeight = BASE_JUMPHEIGHT + jumpForceValue;
     }
 }
 
