a9ff925ecc64c7c7171a278015b4153cb1bcf260
ohirume <0uhirume@gmail.com>
2026-01-31 16:24:10 +0800

    feat: attach visual effect to torso instead of root part

--- src/server/systems/training/visuals/spawn.ts
+++ src/server/systems/training/visuals/spawn.ts
@@ -5,7 +5,7 @@ import { ReplicatedStorage } from "@rbxts/services";
 
 import {
     CharacterInstance,
-    HumanoidRootInstance,
+    TorsoInstance,
     VisualEffectInstance,
 } from "../../../../shared/components";
 import { PowerTrainingEffect } from "../../../../shared/tags";
@@ -27,19 +27,19 @@ for (const [_, model] of pairs(EFFECTS)) {
 
 function system(world: World): void {
     for (const [tag, model] of pairs(EFFECTS)) {
-        for (const [entity, characterInstance, rootInstance] of world
-            .query(CharacterInstance, HumanoidRootInstance)
+        for (const [entity, characterInstance, torsoInstance] of world
+            .query(CharacterInstance, TorsoInstance)
             .with(tag)
             .without(pair(VisualEffectInstance, tag))) {
             const newModel = model.Clone();
             newModel.Name = world.get(tag, Name) ?? "Effect";
             newModel.Parent = characterInstance;
 
-            newModel.PivotTo(rootInstance.CFrame);
+            newModel.PivotTo(torsoInstance.CFrame);
 
             const weld = new Instance("WeldConstraint");
             weld.Part0 = newModel.PrimaryPart;
-            weld.Part1 = rootInstance;
+            weld.Part1 = torsoInstance;
             weld.Parent = newModel.PrimaryPart;
 
             setPairValue(world, entity, VisualEffectInstance, tag, newModel);
