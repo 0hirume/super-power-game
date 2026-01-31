6ad0a98f7148970fc67ececd570412729ed1dd5c
ohirume <0uhirume@gmail.com>
2026-01-31 15:19:06 +0800

    fixup! refactor: move training effects systems into directory

--- src/server/systems/training/index.ts
+++ src/server/systems/training/index.ts
@@ -1,5 +1,4 @@
-export * from "./apply-effects";
-export * from "./remove-effects";
+export * from "./effects";
 export * from "./requests";
 export * from "./update-train-mode";
 export * from "./visuals";
