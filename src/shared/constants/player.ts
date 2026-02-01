8a53f63be51875444dc725a2ee71dd94ddb2ae94
ohirume <0uhirume@gmail.com>
2026-02-01 23:45:10 +0800

    refactor: remove redundant constants and namespace all

--- src/shared/constants/player.ts
+++ src/shared/constants/player.ts
@@ -1,19 +1,9 @@
-export const DEFAULT_HEALTH = 100;
+export namespace BASE {
+    export const HEALTH = 100;
+    export const WALKSPEED = 15;
+    export const JUMPHEIGHT = 5;
+    export const VALUE = 0;
+    export const MULTIPLIER = 1;
+}
 
-export const DEFAULT_STRENGTH = 0;
-export const DEFAULT_STRENGTH_MULTIPLIER = 1;
-export const DEFAULT_ENDURANCE = 0;
-export const DEFAULT_ENDURANCE_MULTIPLIER = 1;
-export const DEFAULT_SPEED = 0;
-export const DEFAULT_SPEED_MULTIPLIER = 1;
-export const DEFAULT_JUMP_FORCE = 0;
-export const DEFAULT_JUMP_FORCE_MULTIPLIER = 1;
-export const DEFAULT_POWER = 0;
-export const DEFAULT_POWER_MULTIPLIER = 1;
-
-export const BASE_MAX_HEALTH = 100;
-export const BASE_WALKSPEED = 15;
-export const BASE_JUMPHEIGHT = 5;
-
-export const TRAINING_COOLDOWN = 1;
 export const COOLDOWN_ZERO = 0;
