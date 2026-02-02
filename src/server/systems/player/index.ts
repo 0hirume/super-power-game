960ea2e0fa922cfa4cf72147c9503ffa7390be17
ohirume <0uhirume@gmail.com>
2026-02-02 11:12:15 +0800

    refactor: rename humanoid-root to root

--- src/server/systems/player/index.ts
+++ src/server/systems/player/index.ts
@@ -2,6 +2,6 @@ export * from "./animator";
 export * from "./character";
 export * from "./cleanup";
 export * from "./humanoid";
-export * from "./humanoid-root";
+export * from "./root";
 export * from "./setup";
 export * from "./torso";
